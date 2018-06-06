659a0e19f50e9de32ff6c4a9c55d48bced0d1b3c
ma1co <ma1co@users.noreply.github.com>
2018-06-06 08:59:08 +0000

    CXD4132: Initial support, enough to boot updater kernel

---
+++ hw/arm/bionz.c
@@ -0,0 +1,128 @@
+/* QEMU model of the Sony BIONZ CXD4132 image processor */
+
+#include "qemu/osdep.h"
+#include "cpu.h"
+#include "hw/boards.h"
+#include "hw/loader.h"
+#include "hw/sysbus.h"
+#include "hw/char/pl011.h"
+#include "hw/cpu/arm11mpcore.h"
+#include "qapi/error.h"
+#include "sysemu/sysemu.h"
+
+#define CXD4132_DDR_BASE 0x80000000
+#define CXD4132_DDR_SIZE 0x20000000
+
+#define CXD4132_SRAM_BASE 0xa0000000
+#define CXD4132_SRAM_SIZE 0x00400000
+
+#define CXD4132_HWTIMER_BASE(i) (0xf2008000 + (i) * 0x20)
+#define CXD4132_NUM_HWTIMER 5
+
+#define CXD4132_UART_BASE(i) (0xf2038000 + (i) * 0x1000)
+#define CXD4132_NUM_UART 3
+
+#define CXD4132_MISCCTRL_BASE 0xf3060000
+
+#define CXD4132_MPCORE_BASE 0xf8000000
+
+#define CXD4132_IRQ_UART(i) (160 + (i))
+#define CXD4132_IRQ_HWTIMER(i) (163 + (i))
+
+#define CXD4132_NUM_IRQ 256
+#define CXD4132_IRQ_OFFSET 32
+
+#define CXD4132_TEXT_OFFSET 0x00018000
+#define CXD4132_INITRD_OFFSET 0x00408000
+
+#define TYPE_CXD4132 MACHINE_TYPE_NAME("cxd4132")
+#define CXD4132(obj) OBJECT_CHECK(Cxd4132State, (obj), TYPE_CXD4132)
+
+typedef struct Cxd4132State {
+    MachineState parent_obj;
+    ARMCPU cpu;
+    hwaddr loader_base;
+} Cxd4132State;
+
+static void cxd4132_reset(void *opaque)
+{
+    Cxd4132State *s = CXD4132(opaque);
+    cpu_reset(CPU(&s->cpu));
+    cpu_set_pc(CPU(&s->cpu), s->loader_base);
+}
+
+static void cxd4132_init(MachineState *machine)
+{
+    Cxd4132State *s = CXD4132(machine);
+    MemoryRegion *mem;
+    DeviceState *dev;
+    qemu_irq irq[CXD4132_NUM_IRQ - CXD4132_IRQ_OFFSET];
+    int i;
+
+    object_initialize(&s->cpu, sizeof(s->cpu), machine->cpu_type);
+    qdev_init_nofail(DEVICE(&s->cpu));
+
+    mem = g_new(MemoryRegion, 1);
+    memory_region_init_ram(mem, NULL, "ddr", CXD4132_DDR_SIZE, &error_fatal);
+    memory_region_add_subregion(get_system_memory(), CXD4132_DDR_BASE, mem);
+
+    mem = g_new(MemoryRegion, 1);
+    memory_region_init_ram(mem, NULL, "sram", CXD4132_SRAM_SIZE, &error_fatal);
+    memory_region_add_subregion(get_system_memory(), CXD4132_SRAM_BASE, mem);
+
+    dev = qdev_create(NULL, TYPE_ARM11MPCORE_PRIV);
+    qdev_prop_set_uint32(dev, "num-cpu", 1);
+    qdev_prop_set_uint32(dev, "num-irq", CXD4132_NUM_IRQ);
+    qdev_init_nofail(dev);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, CXD4132_MPCORE_BASE);
+    sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, qdev_get_gpio_in(DEVICE(&s->cpu), ARM_CPU_IRQ));
+    for (i = 0; i < CXD4132_NUM_IRQ - CXD4132_IRQ_OFFSET; i++) {
+        irq[i] = qdev_get_gpio_in(dev, i);
+    }
+
+    for (i = 0; i < CXD4132_NUM_HWTIMER; i++) {
+        dev = qdev_create(NULL, "bionz_hwtimer");
+        qdev_init_nofail(dev);
+        sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, CXD4132_HWTIMER_BASE(i));
+        sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, irq[CXD4132_IRQ_HWTIMER(i) - CXD4132_IRQ_OFFSET]);
+    }
+
+    for (i = 0; i < CXD4132_NUM_UART; i++) {
+        pl011_create(CXD4132_UART_BASE(i), irq[CXD4132_IRQ_UART(i) - CXD4132_IRQ_OFFSET], serial_hds[i]);
+    }
+
+    dev = qdev_create(NULL, "bionz_miscctrl");
+    qdev_prop_set_uint32(dev, "typeid", 0x301);
+    qdev_init_nofail(dev);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, CXD4132_MISCCTRL_BASE);
+
+    load_image_targphys(machine->kernel_filename, CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET, CXD4132_DDR_SIZE - CXD4132_TEXT_OFFSET);
+    load_image_targphys(machine->initrd_filename, CXD4132_DDR_BASE + CXD4132_INITRD_OFFSET, CXD4132_DDR_SIZE - CXD4132_INITRD_OFFSET);
+
+    s->loader_base = CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET;
+    qemu_register_reset(cxd4132_reset, s);
+}
+
+static void cxd4132_class_init(ObjectClass *klass, void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(klass);
+
+    mc->desc = "Sony BIONZ CXD4132";
+    mc->init = cxd4132_init;
+    mc->default_cpu_type = ARM_CPU_TYPE_NAME("arm11mpcore");
+    mc->ignore_memory_transaction_failures = true;
+}
+
+static const TypeInfo cxd4132_info = {
+    .name          = TYPE_CXD4132,
+    .parent        = TYPE_MACHINE,
+    .instance_size = sizeof(Cxd4132State),
+    .class_init    = cxd4132_class_init,
+};
+
+static void cxd4132_register_type(void)
+{
+    type_register_static(&cxd4132_info);
+}
+
+type_init(cxd4132_register_type)
---
+++ hw/timer/bionz_hwtimer.c
@@ -0,0 +1,195 @@
+/* QEMU model of the Sony BIONZ timer block */
+
+#include "qemu/osdep.h"
+#include "hw/sysbus.h"
+#include "qemu/log.h"
+#include "sysemu/sysemu.h"
+
+#define TIMERCTL 0x00
+#define TIMERCLR 0x04
+#define TIMERCMP 0x08
+#define TIMERREAD 0x0c
+#define TIMERLOAD 0x10
+
+#define CTL_RST(reg) ((reg) & 0x80000000)
+#define CTL_EN(reg) ((reg) & 0x1000)
+#define CTL_IEN(reg) ((reg) & 0x100)
+#define CTL_MODE(reg) ((reg) & 0x30)
+#define CTL_PER(reg) (250 << ((reg) & 0x7))
+
+#define CLR_CLR(reg) ((reg) & 0x10)
+#define CLR_INTCLR(reg) ((reg) & 0x1)
+
+#define MODE_PERIODIC 0x00
+#define MODE_ONESHOT 0x10
+#define MODE_FREERUN 0x30
+
+#define TYPE_BIONZ_HWTIMER "bionz_hwtimer"
+#define BIONZ_HWTIMER(obj) OBJECT_CHECK(HwtimerState, (obj), TYPE_BIONZ_HWTIMER)
+
+typedef struct HwtimerState {
+    SysBusDevice parent_obj;
+    MemoryRegion mmio;
+    qemu_irq intr;
+
+    QEMUTimer *timer;
+    int64_t last_tick;
+    int64_t next_tick;
+
+    uint32_t reg_ctl;
+    uint32_t reg_cmp;
+    uint32_t reg_value;
+} HwtimerState;
+
+static void hwtimer_reload(HwtimerState *s)
+{
+    if (CTL_EN(s->reg_ctl)) {
+        s->next_tick = s->last_tick + (s->reg_cmp - s->reg_value) * CTL_PER(s->reg_ctl);
+        timer_mod(s->timer, s->next_tick);
+    } else {
+        s->next_tick = 0;
+        timer_del(s->timer);
+    }
+}
+
+static void hwtimer_tick(void *opaque)
+{
+    HwtimerState *s = BIONZ_HWTIMER(opaque);
+
+    s->reg_value += (s->next_tick - s->last_tick) / CTL_PER(s->reg_ctl) + 1;
+    s->last_tick = s->next_tick;
+
+    if (CTL_MODE(s->reg_ctl) == MODE_ONESHOT) {
+        s->reg_ctl &= ~CTL_EN(s->reg_ctl);
+    } else if (CTL_MODE(s->reg_ctl) == MODE_PERIODIC) {
+        s->reg_value = 0;
+    }
+    hwtimer_reload(s);
+
+    if (CTL_IEN(s->reg_ctl)) {
+        qemu_irq_raise(s->intr);
+    }
+}
+
+static void hwtimer_reset(DeviceState *dev)
+{
+    HwtimerState *s = BIONZ_HWTIMER(dev);
+
+    s->last_tick = 0;
+    s->next_tick = 0;
+    s->reg_ctl = 0;
+    s->reg_cmp = 0xffffffff;
+    s->reg_value = 0;
+    hwtimer_reload(s);
+}
+
+static uint64_t hwtimer_read(void *opaque, hwaddr offset, unsigned size)
+{
+    HwtimerState *s = BIONZ_HWTIMER(opaque);
+
+    switch (offset) {
+        case TIMERCTL:
+            return s->reg_ctl;
+
+        case TIMERCMP:
+            return s->reg_cmp;
+
+        case TIMERREAD:
+            if (CTL_EN(s->reg_ctl)) {
+                return s->reg_value + (qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) - s->last_tick) / CTL_PER(s->reg_ctl);
+            } else {
+                return s->reg_value;
+            }
+
+        default:
+            qemu_log_mask(LOG_UNIMP, "%s: unimplemented read @ 0x%" HWADDR_PRIx "\n", __func__, offset);
+            return 0;
+    }
+}
+
+static void hwtimer_write(void *opaque, hwaddr offset, uint64_t value, unsigned size)
+{
+    HwtimerState *s = BIONZ_HWTIMER(opaque);
+
+    switch (offset) {
+        case TIMERCTL:
+            if (CTL_RST(value)) {
+                hwtimer_reset(DEVICE(s));
+            }
+            if (!CTL_EN(s->reg_ctl)) {
+                s->last_tick = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+            }
+            s->reg_ctl = value & 0xffff;
+            hwtimer_reload(s);
+            break;
+
+        case TIMERCLR:
+            if (CLR_CLR(value)) {
+                s->reg_value = 0;
+                s->last_tick = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+                hwtimer_reload(s);
+            }
+            if (CLR_INTCLR(value)) {
+                qemu_irq_lower(s->intr);
+            }
+            break;
+
+        case TIMERCMP:
+            s->reg_cmp = value;
+            hwtimer_reload(s);
+            break;
+
+        case TIMERLOAD:
+            s->reg_value = value;
+            s->last_tick = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+            hwtimer_reload(s);
+            break;
+
+        default:
+            qemu_log_mask(LOG_UNIMP, "%s: unimplemented write @ 0x%" HWADDR_PRIx ": 0x%" PRIx64 "\n", __func__, offset, value);
+    }
+}
+
+static const struct MemoryRegionOps hwtimer_ops = {
+    .read = hwtimer_read,
+    .write = hwtimer_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+    .valid.min_access_size = 4,
+    .valid.max_access_size = 4,
+};
+
+static int hwtimer_init(SysBusDevice *sbd)
+{
+    HwtimerState *s = BIONZ_HWTIMER(sbd);
+
+    s->timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, hwtimer_tick, s);
+
+    memory_region_init_io(&s->mmio, OBJECT(sbd), &hwtimer_ops, s, TYPE_BIONZ_HWTIMER, 0x20);
+    sysbus_init_mmio(sbd, &s->mmio);
+    sysbus_init_irq(sbd, &s->intr);
+
+    return 0;
+}
+
+static void hwtimer_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
+
+    k->init = hwtimer_init;
+    dc->reset = hwtimer_reset;
+}
+
+static const TypeInfo hwtimer_info = {
+    .name          = TYPE_BIONZ_HWTIMER,
+    .parent        = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(HwtimerState),
+    .class_init    = hwtimer_class_init,
+};
+
+static void hwtimer_register_type(void)
+{
+    type_register_static(&hwtimer_info);
+}
+
+type_init(hwtimer_register_type)
