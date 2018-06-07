435989b8ea195f36047398e164aca0326647aef5
ma1co <ma1co@users.noreply.github.com>
2018-06-07 07:02:40 +0000

    CXD4132: Load bootloader from nand if no kernel is specified

--- hw/arm/bionz.c
+++ hw/arm/bionz.c
@@ -9,6 +9,7 @@
 #include "hw/cpu/arm11mpcore.h"
 #include "hw/loader.h"
 #include "qapi/error.h"
+#include "sysemu/block-backend.h"
 #include "sysemu/sysemu.h"
 
 #define CXD4132_NAND_BASE 0x00000000
@@ -43,6 +44,8 @@
 #define CXD4132_TEXT_OFFSET 0x00018000
 #define CXD4132_INITRD_OFFSET 0x00408000
 
+#define NAND_SECTOR_SIZE 0x200
+
 #define TYPE_CXD4132 MACHINE_TYPE_NAME("cxd4132")
 #define CXD4132(obj) OBJECT_CHECK(Cxd4132State, (obj), TYPE_CXD4132)
 
@@ -72,6 +75,38 @@ static void cxd4132_reset(void *opaque)
     Cxd4132State *s = CXD4132(opaque);
     cpu_reset(CPU(&s->cpu));
     cpu_set_pc(CPU(&s->cpu), s->loader_base);
+}
+
+static uint32_t cxd4132_init_loader(Cxd4132State *s)
+{
+    char boot_block[NAND_SECTOR_SIZE];
+    uint32_t loader_offset, loader_size, loader_base;
+    void *loader_buffer;
+
+    if (!s->drive) {
+        return 0;
+    }
+
+    if (blk_pread(s->drive, 0, boot_block, sizeof(boot_block)) < 0) {
+        hw_error("%s: Cannot read boot block\n", __func__);
+    }
+
+    if (*(uint32_t *) boot_block != *(uint32_t *) "EXBL") {
+        hw_error("%s: Wrong boot block signature\n", __func__);
+    }
+
+    loader_offset = (*(uint32_t *) (boot_block + 0x40)) * NAND_SECTOR_SIZE;
+    loader_size = (*(uint32_t *) (boot_block + 0x44)) * NAND_SECTOR_SIZE;
+    loader_base = *(uint32_t *) (boot_block + 0x50);
+
+    loader_buffer = g_malloc(loader_size);
+    if (blk_pread(s->drive, loader_offset, loader_buffer, loader_size) < 0) {
+        hw_error("%s: Cannot read loader\n", __func__);
+    }
+    rom_add_blob_fixed("loader", loader_buffer, loader_size, loader_base);
+    g_free(loader_buffer);
+
+    return loader_base;
 }
 
 static void cxd4132_init_cmdline(const char *default_cmdline, const char *cmdline, hwaddr base)
@@ -154,11 +189,15 @@ static void cxd4132_init(MachineState *machine)
     qdev_init_nofail(dev);
     sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, CXD4132_MISCCTRL_BASE);
 
-    load_image_targphys(machine->kernel_filename, CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET, CXD4132_DDR_SIZE - CXD4132_TEXT_OFFSET);
-    load_image_targphys(machine->initrd_filename, CXD4132_DDR_BASE + CXD4132_INITRD_OFFSET, CXD4132_DDR_SIZE - CXD4132_INITRD_OFFSET);
-    cxd4132_init_cmdline(CXD4132_CMDLINE, machine->kernel_cmdline, CXD4132_DDR_BASE + CXD4132_CMDLINE_OFFSET);
+    if (machine->kernel_filename) {
+        load_image_targphys(machine->kernel_filename, CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET, CXD4132_DDR_SIZE - CXD4132_TEXT_OFFSET);
+        load_image_targphys(machine->initrd_filename, CXD4132_DDR_BASE + CXD4132_INITRD_OFFSET, CXD4132_DDR_SIZE - CXD4132_INITRD_OFFSET);
+        cxd4132_init_cmdline(CXD4132_CMDLINE, machine->kernel_cmdline, CXD4132_DDR_BASE + CXD4132_CMDLINE_OFFSET);
+        s->loader_base = CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET;
+    } else {
+        s->loader_base = cxd4132_init_loader(s);
+    }
 
-    s->loader_base = CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET;
     qemu_register_reset(cxd4132_reset, s);
 }
 
