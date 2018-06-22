6cc23de291962c4356b11da9b93df460d77e4bdc
ma1co <ma1co@users.noreply.github.com>
2018-06-22 17:33:08 +0000

    CXD4115: Initial support, enough to boot updater kernel

--- hw/arm/bionz.c
+++ hw/arm/bionz.c
@@ -12,6 +12,26 @@
 #include "sysemu/block-backend.h"
 #include "sysemu/sysemu.h"
 
+//////////////////////////// CXD4115 ////////////////////////////
+#define CXD4115_DDR_BASE 0x10000000
+#define CXD4115_DDR_SIZE 0x10000000
+#define CXD4115_HWTIMER_BASE(i) (0x7a000000 + (i) * 0x20)
+#define CXD4115_NUM_HWTIMER 3
+#define CXD4115_UART_BASE(i) (0x7a050000 + (i) * 0x1000)
+#define CXD4115_NUM_UART 3
+#define CXD4115_SRAM_BASE 0xfff00000
+#define CXD4115_SRAM_SIZE 0x00008000
+#define CXD4115_MPCORE_BASE 0xfffd0000
+
+#define CXD4115_NUM_IRQ 256
+#define CXD4115_IRQ_OFFSET 32
+#define CXD4115_IRQ_UART(i) (152 + (i))
+#define CXD4115_IRQ_HWTIMER(i) (155 + (i))
+
+#define CXD4115_BOOT_DEVICE_OFFSET 0x0000236c
+#define CXD4115_TEXT_OFFSET 0x00208000
+#define CXD4115_INITRD_OFFSET 0x0062e000
+
 //////////////////////////// CXD4132 ////////////////////////////
 #define CXD4132_NAND_BASE 0x00000000
 #define CXD4132_DDR_BASE 0x80000000
@@ -123,6 +143,58 @@ static void cxd_init_cmdline(const char *default_cmdline, const char *cmdline, h
     g_free(buf);
 }
 
+static void cxd4115_init(MachineState *machine)
+{
+    CxdState *s = CXD(machine);
+    MemoryRegion *mem;
+    DeviceState *dev;
+    qemu_irq irq[CXD4115_NUM_IRQ - CXD4115_IRQ_OFFSET];
+    int i;
+
+    object_initialize(&s->cpu, sizeof(s->cpu), machine->cpu_type);
+    qdev_init_nofail(DEVICE(&s->cpu));
+
+    mem = g_new(MemoryRegion, 1);
+    memory_region_init_ram(mem, NULL, "ddr", CXD4115_DDR_SIZE, &error_fatal);
+    memory_region_add_subregion(get_system_memory(), CXD4115_DDR_BASE, mem);
+
+    mem = g_new(MemoryRegion, 1);
+    memory_region_init_ram(mem, NULL, "sram", CXD4115_SRAM_SIZE, &error_fatal);
+    memory_region_add_subregion(get_system_memory(), CXD4115_SRAM_BASE, mem);
+
+    dev = qdev_create(NULL, TYPE_ARM11MPCORE_PRIV);
+    qdev_prop_set_uint32(dev, "num-cpu", 1);
+    qdev_prop_set_uint32(dev, "num-irq", CXD4115_NUM_IRQ);
+    qdev_init_nofail(dev);
+    sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, CXD4115_MPCORE_BASE);
+    sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, qdev_get_gpio_in(DEVICE(&s->cpu), ARM_CPU_IRQ));
+    for (i = 0; i < CXD4115_NUM_IRQ - CXD4115_IRQ_OFFSET; i++) {
+        irq[i] = qdev_get_gpio_in(dev, i);
+    }
+
+    for (i = 0; i < CXD4115_NUM_HWTIMER; i++) {
+        dev = qdev_create(NULL, "bionz_hwtimer");
+        qdev_init_nofail(dev);
+        sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, CXD4115_HWTIMER_BASE(i));
+        sysbus_connect_irq(SYS_BUS_DEVICE(dev), 0, irq[CXD4115_IRQ_HWTIMER(i) - CXD4115_IRQ_OFFSET]);
+    }
+
+    for (i = 0; i < CXD4115_NUM_UART; i++) {
+        pl011_create(CXD4115_UART_BASE(i), irq[CXD4115_IRQ_UART(i) - CXD4115_IRQ_OFFSET], serial_hds[i]);
+    }
+
+    if (machine->kernel_filename) {
+        load_image_targphys(machine->kernel_filename, CXD4115_DDR_BASE + CXD4115_TEXT_OFFSET, CXD4115_DDR_SIZE - CXD4115_TEXT_OFFSET);
+        load_image_targphys(machine->initrd_filename, CXD4115_DDR_BASE + CXD4115_INITRD_OFFSET, CXD4115_DDR_SIZE - CXD4115_INITRD_OFFSET);
+        s->loader_base = CXD4115_DDR_BASE + CXD4115_TEXT_OFFSET;
+
+        uint32_t boot_device = 1;
+        rom_add_blob_fixed("boot_device", &boot_device, sizeof(boot_device), CXD4115_DDR_BASE + CXD4115_BOOT_DEVICE_OFFSET);
+    }
+
+    qemu_register_reset(cxd_reset, s);
+}
+
 static void cxd4132_init(MachineState *machine)
 {
     CxdState *s = CXD(machine);
@@ -227,6 +299,29 @@ static void cxd_register_type(void)
 }
 
 type_init(cxd_register_type)
+
+static void cxd4115_class_init(ObjectClass *klass, void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(klass);
+
+    mc->desc = "Sony BIONZ CXD4115";
+    mc->init = cxd4115_init;
+    mc->default_cpu_type = ARM_CPU_TYPE_NAME("arm11mpcore");
+    mc->ignore_memory_transaction_failures = true;
+}
+
+static const TypeInfo cxd4115_info = {
+    .name          = MACHINE_TYPE_NAME("cxd4115"),
+    .parent        = TYPE_CXD,
+    .class_init    = cxd4115_class_init,
+};
+
+static void cxd4115_register_type(void)
+{
+    type_register_static(&cxd4115_info);
+}
+
+type_init(cxd4115_register_type)
 
 static void cxd4132_class_init(ObjectClass *klass, void *data)
 {
