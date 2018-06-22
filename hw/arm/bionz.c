52c8ccaa5f8de84ba8c23af12b4708e2a8992156
ma1co <ma1co@users.noreply.github.com>
2018-06-22 17:18:33 +0000

    prepare addition of more bionz machines

--- hw/arm/bionz.c
+++ hw/arm/bionz.c
@@ -1,4 +1,4 @@
-/* QEMU model of the Sony BIONZ CXD4132 image processor */
+/* QEMU model of Sony BIONZ image processors */
 
 #include "qemu/osdep.h"
 #include "cpu.h"
@@ -12,30 +12,24 @@
 #include "sysemu/block-backend.h"
 #include "sysemu/sysemu.h"
 
+//////////////////////////// CXD4132 ////////////////////////////
 #define CXD4132_NAND_BASE 0x00000000
-
 #define CXD4132_DDR_BASE 0x80000000
 #define CXD4132_DDR_SIZE 0x20000000
-
 #define CXD4132_SRAM_BASE 0xa0000000
 #define CXD4132_SRAM_SIZE 0x00400000
-
 #define CXD4132_USB_BASE 0xf0040000
-
 #define CXD4132_DMA_BASE 0xf2001000
-
 #define CXD4132_MENO_BASE 0xf2002000
-
 #define CXD4132_HWTIMER_BASE(i) (0xf2008000 + (i) * 0x20)
 #define CXD4132_NUM_HWTIMER 5
-
 #define CXD4132_UART_BASE(i) (0xf2038000 + (i) * 0x1000)
 #define CXD4132_NUM_UART 3
-
 #define CXD4132_MISCCTRL_BASE 0xf3060000
-
 #define CXD4132_MPCORE_BASE 0xf8000000
 
+#define CXD4132_NUM_IRQ 256
+#define CXD4132_IRQ_OFFSET 32
 #define CXD4132_IRQ_UART(i) (160 + (i))
 #define CXD4132_IRQ_HWTIMER(i) (163 + (i))
 #define CXD4132_IRQ_DMA(i) (176 + (i))
@@ -43,18 +37,10 @@
 #define CXD4132_IRQ_NAND 183
 #define CXD4132_IRQ_USB 222
 
-#define CXD4132_NUM_IRQ 256
-#define CXD4132_IRQ_OFFSET 32
-
 #define CXD4132_BOOT_DEVICE_OFFSET 0x00005050
 #define CXD4132_CMDLINE_OFFSET 0x00013000
 #define CXD4132_TEXT_OFFSET 0x00018000
 #define CXD4132_INITRD_OFFSET 0x00408000
-
-#define NAND_SECTOR_SIZE 0x200
-
-#define TYPE_CXD4132 MACHINE_TYPE_NAME("cxd4132")
-#define CXD4132(obj) OBJECT_CHECK(Cxd4132State, (obj), TYPE_CXD4132)
 
 #define CXD4132_CMDLINE \
     "lpj=622592 " \
@@ -70,21 +56,26 @@
     "memrsv=32K@0x80000000 " \
     "memrsv=0x1270000@0x82D90000 "
 
-typedef struct Cxd4132State {
+#define NAND_SECTOR_SIZE 0x200
+
+#define TYPE_CXD "cxd"
+#define CXD(obj) OBJECT_CHECK(CxdState, (obj), TYPE_CXD)
+
+typedef struct CxdState {
     MachineState parent_obj;
     ARMCPU cpu;
     BlockBackend *drive;
     hwaddr loader_base;
-} Cxd4132State;
+} CxdState;
 
-static void cxd4132_reset(void *opaque)
+static void cxd_reset(void *opaque)
 {
-    Cxd4132State *s = CXD4132(opaque);
+    CxdState *s = CXD(opaque);
     cpu_reset(CPU(&s->cpu));
     cpu_set_pc(CPU(&s->cpu), s->loader_base);
 }
 
-static uint32_t cxd4132_init_loader(Cxd4132State *s)
+static uint32_t cxd_init_loader(CxdState *s)
 {
     char boot_block[NAND_SECTOR_SIZE];
     uint32_t loader_offset, loader_size, loader_base;
@@ -116,7 +107,7 @@ static uint32_t cxd4132_init_loader(Cxd4132State *s)
     return loader_base;
 }
 
-static void cxd4132_init_cmdline(const char *default_cmdline, const char *cmdline, hwaddr base)
+static void cxd_init_cmdline(const char *default_cmdline, const char *cmdline, hwaddr base)
 {
     const char *header = "kemco ";
     const char *spacer = " ";
@@ -134,7 +125,7 @@ static void cxd4132_init_cmdline(const char *default_cmdline, const char *cmdlin
 
 static void cxd4132_init(MachineState *machine)
 {
-    Cxd4132State *s = CXD4132(machine);
+    CxdState *s = CXD(machine);
     DriveInfo *dinfo;
     MemoryRegion *mem;
     DeviceState *dev;
@@ -211,17 +202,31 @@ static void cxd4132_init(MachineState *machine)
     if (machine->kernel_filename) {
         load_image_targphys(machine->kernel_filename, CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET, CXD4132_DDR_SIZE - CXD4132_TEXT_OFFSET);
         load_image_targphys(machine->initrd_filename, CXD4132_DDR_BASE + CXD4132_INITRD_OFFSET, CXD4132_DDR_SIZE - CXD4132_INITRD_OFFSET);
-        cxd4132_init_cmdline(CXD4132_CMDLINE, machine->kernel_cmdline, CXD4132_DDR_BASE + CXD4132_CMDLINE_OFFSET);
+        cxd_init_cmdline(CXD4132_CMDLINE, machine->kernel_cmdline, CXD4132_DDR_BASE + CXD4132_CMDLINE_OFFSET);
         s->loader_base = CXD4132_DDR_BASE + CXD4132_TEXT_OFFSET;
 
         uint32_t boot_device = 1;
         rom_add_blob_fixed("boot_device", &boot_device, sizeof(boot_device), CXD4132_DDR_BASE + CXD4132_BOOT_DEVICE_OFFSET);
     } else {
-        s->loader_base = cxd4132_init_loader(s);
+        s->loader_base = cxd_init_loader(s);
     }
 
-    qemu_register_reset(cxd4132_reset, s);
+    qemu_register_reset(cxd_reset, s);
 }
+
+static const TypeInfo cxd_info = {
+    .name          = TYPE_CXD,
+    .parent        = TYPE_MACHINE,
+    .abstract      = true,
+    .instance_size = sizeof(CxdState),
+};
+
+static void cxd_register_type(void)
+{
+    type_register_static(&cxd_info);
+}
+
+type_init(cxd_register_type)
 
 static void cxd4132_class_init(ObjectClass *klass, void *data)
 {
@@ -234,9 +239,8 @@ static void cxd4132_class_init(ObjectClass *klass, void *data)
 }
 
 static const TypeInfo cxd4132_info = {
-    .name          = TYPE_CXD4132,
-    .parent        = TYPE_MACHINE,
-    .instance_size = sizeof(Cxd4132State),
+    .name          = MACHINE_TYPE_NAME("cxd4132"),
+    .parent        = TYPE_CXD,
     .class_init    = cxd4132_class_init,
 };
 
